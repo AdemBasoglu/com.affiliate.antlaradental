947d618bc8ee4b77b9cbfb779d88a9f0a117d2dc
Adem Basoglu <admbasoglu28@gmail.com>
2023-12-27 22:35:10 +0100

    update

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -1,47 +1,93 @@
 package utilities;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
+
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 public class Driver {
-    static WebDriver driver;
+    //create a driver instance
+    public static WebDriver driver;
+    public static int timeout = 5;
+
+    //What?=>It is just to create, initialize the driver instance.(Singleton driver)
+    //Why?=>We don't want to create and initialize the driver when we don't need
+    //We will create and initialize the driver when it is null
+    //We can use Driver class with different browser(chrome,firefox,headless)
+    private Driver() {
+        //we don't want to create another abject. Singleton pattern
+    }
+
+    //to initialize the driver we create a static method
     public static WebDriver getDriver() {
-        if (driver == null){
-            switch (ConfigReader.getProperty("browser")){
-                case "chrome":
-                    driver = new ChromeDriver();
-                    break;
-                case "edge":
-                    driver = new EdgeDriver();
-                    break;
-                case "safari":
-                    driver = new SafariDriver();
-                    break;
-                default:
-                    driver=new InternetExplorerDriver();
+        //create the driver if and only if it is null
+        if (driver == null) {
+            String browser = ConfigReader.getProperty("browser");
+            if ("chrome".equals(browser)) {
+                driver = new ChromeDriver();
+                //driver.manage().window().maximize();
+
+            } else if ("firefox".equals(browser)) {
+                driver = new FirefoxDriver();
+            } else if ("ie".equals(browser)) {
+                driver = new InternetExplorerDriver();
+            } else if ("safari".equals(browser)) {
+                driver = new SafariDriver();
+            } else if ("chrome-headless".equals(browser)) {
+                //    driver = new ChromeDriver(new ChromeOptions().setHeadless(true));
             }
         }
+        //    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
         return driver;
     }
-    public static void closeDriver(){
-        if (driver != null){
-            driver.close();
-            driver = null;
+
+    public static void closeDriver() {
+        if (driver != null) {//if the driver is pointing chrome
+            driver.quit();//quit the driver
+            driver = null;//set it back to null to make sure driver is null
+            // so I can initialize it again
+            //This is important especially you do cross browser testing(testing with
+            // multiple browser like chrome, firefox, ie etc.)
         }
     }
-    public static void quitDriver(){
-        if (driver != null){
-            driver.quit();
-            driver = null;
-        }
+
+    //    Driver.selectByVisibleText(dropdown element, "CHECKING-91303-116.98$")
+    public static void selectByVisibleText(WebElement element, String text) {
+        Select objSelect = new Select(element);
+        objSelect.selectByVisibleText(text);
     }
+    //    Parameter1 : WebElement
+//    Parameter2:  int
+//    Driver.selectByIndex(dropdown element, 1)
+    public static void selectByIndex(WebElement element, int index) {
+        Select objSelect = new Select(element);
+        objSelect.selectByIndex(index);
+    }
+    //    Parameter1 : WebElement
+//    Parameter2:  String
+//    Driver.selectByIndex(dropdown element, "91303")
+    public static void selectByValue(WebElement element, String value) {
+        Select objSelect = new Select(element);
+        List<WebElement> elementCount = objSelect.getOptions();
+        objSelect.selectByValue(value);
+        System.out.println("number of elements: " + elementCount.size());
+}
+
+
+}
 
 
 
@@ -54,7 +100,7 @@ public class Driver {
         // obje olusturma ile bu method'larin kullanilmamasi
         // veya obje olusturularak farkli uygulamalarin
         // POM icinde kullanilmasini engellemek icin Singleton Pattern tercih edilmistir.
-    }
+
 
     /* cagrildigi yere bir WebDriver objesi dondurecek
        getDriver() olusturalim
