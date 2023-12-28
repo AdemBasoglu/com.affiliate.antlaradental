6a9d5943651a24924d21676d59bba5565c397d5b
Ercan Bayraktar <ercan.byrktr.545435@gmail.com>
2023-12-28 21:43:21 +0100

    ercan_US_09_v1

--- src/test/java/stepdefinitions/ExampleStepdefinitions.java
+++ src/test/java/stepdefinitions/ExampleStepdefinitions.java
@@ -1,6 +1,8 @@
 package stepdefinitions;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
 import pages.ExamplePage;
 import utilities.ConfigReader;
 import utilities.Driver;
@@ -13,6 +15,30 @@ public class ExampleStepdefinitions {
  @Given("user goes the check24 home page")
  public void user_goes_the_check24_home_page() {
   Driver.getDriver().get(ConfigReader.getProperty("toUrl"));  throw new io.cucumber.java.PendingException();
+
  }
+
+ @Then("cookies accept")
+ public void cookies_accept() {
+   examplePage.cookiesAccept.click();
+ }
+
+ @Then("Click to Merkzettel")
+ public void click_to_merkzettel() {
+  examplePage.linkMerkzettel.click();
+ }
+
+ @Then("Merkzettel is displayed")
+ public void merkzettel_is_displayed() {
+  Assert.assertTrue(examplePage.linkNoteList.isDisplayed());
+
+ }
+
+ @Then("close page")
+ public void close_page() {
+
+ }
+
+
 
 }
