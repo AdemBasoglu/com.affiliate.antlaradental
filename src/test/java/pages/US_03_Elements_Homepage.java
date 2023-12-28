93600b36f98e66429964f314cfc75b1dcb542eef
Adem Basoglu <admbasoglu28@gmail.com>
2023-12-28 23:06:15 +0100

    a

--- src/test/java/pages/US_03_Elements_Homepage.java
+++ src/test/java/pages/US_03_Elements_Homepage.java
@@ -13,20 +13,20 @@ public class US_03_Elements_Homepage extends ReusableMethods {
 
     public US_03_Elements_Homepage() {PageFactory.initElements(Driver.getDriver(),this);}
 
-    @FindBy(className = "c24-nav-ele c24-nav-")
-    private List<WebElement> elements;
+  //@FindBy(className = "c24-nav-ele c24-nav-")
+  //private List<WebElement> elements;
 
-    @FindBy(className = "c24-subnav c24-hover-layer clearfix")
-    private List<WebElement> elements_dropDownsMenu;
-
-
+  //@FindBy(className = "c24-subnav c24-hover-layer clearfix")
+  //private List<WebElement> elements_dropDownsMenu;
 
 
-    public List<WebElement> getElements() {
-        return elements;
-    }
 
-    public List<WebElement> getElements_dropDownsMenu() {
-        return elements_dropDownsMenu;
-    }
+
+   // public List<WebElement> getElements() {
+   //     return elements;
+   // }
+//
+   // public List<WebElement> getElements_dropDownsMenu() {
+   //     return elements_dropDownsMenu;
+   // }
 }
