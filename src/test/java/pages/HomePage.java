e00929ec08ac6057283485aa0a11e83913ea1d1c
ilyas karakoc <ilyas.karakoc79@gmail.com>
2023-12-30 18:43:24 +0100

    Ilyas-30.12

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -8,6 +8,7 @@ import utilities.Driver;
 public class HomePage {
 
     public HomePage() {
+
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
@@ -18,8 +19,17 @@ public class HomePage {
     @FindBy(xpath = "//span[@class='c24-customer-hover c24-header-hover'] ")
     public WebElement meinKontoLink;
 
-    @FindBy(xpath = "//*[text()='Starten Sie hier.'] ")
+    @FindBy(xpath = "//a[@class='c24-meinkonto-reflink c24-meinkonto-register-link c24-uliloginlayer-register-opener'] ")
     public WebElement startenSieHierLink;
+    @FindBy(xpath = "(//*[text()='Nachhaltigkeit'][1])[1]")
+    public WebElement Nachhaltigkeit;
 
+    @FindBy(xpath = "//*[text()='Nachhaltige Unternehmensführung']")
+    public WebElement NUnternehmensführung;
+
+    @FindBy(xpath = "//*[text()='Ausgezeichnetes Engagement']")
+    public WebElement Ausgezeichnetes;
+    @FindBy(xpath = "//*[@class='c24-search-input hits-search-element']")
+    public WebElement searchBox ;
 
 }
