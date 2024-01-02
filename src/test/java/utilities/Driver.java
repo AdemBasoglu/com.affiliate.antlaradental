8ef2154c1b34be711eb337d565a3a5fd749a3651
Kok <ayten.kok@accenture.com>
2024-01-02 09:10:43 +0100

    Çakışmaları çözüldü

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -1,5 +1,9 @@
 package utilities;
 
+
+import
+        io.github.bonigarcia.wdm.WebDriverManager;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
